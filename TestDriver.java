4d5549df19b0c85f3a9a2715ecc5e98ef6cf377a
Andrew Pham <andrewdpham3@gmail.com>
2020-09-12 16:24:58 -0700

    keep ledger in memory

--- TestDriver.java
+++ TestDriver.java
@@ -32,22 +32,21 @@ class Ledger {
         account.balance = 0;
         return accountId;
     }
-    /*
-	String processTransaction(Transaction transaction) {
-		return transaction.transactionId;
-	}
-	int getAccountBalance(String address) {
-		return balance;
-	}
-	Map getAccountBalances(){
-		return accountbalances;
-	}
-	Block getBlock(int blockNumber) {
-		return block;
-	}
-	Transaction getTransaction(String transactionId) {
-		return transaction;
-	}*/
+    /*String processTransaction(Transaction transaction) {
+        return transaction.transactionId;
+    }
+    int getAccountBalance(String address) {
+        return balance;
+    }
+    Map getAccountBalances() {
+        return accountbalances;
+    }
+    Block getBlock(int blockNumber) {
+        return block;
+    }
+    Transaction getTransaction(String transactionId) {
+        return transaction;
+    }*/
     void validate() {
     }
 }
@@ -56,26 +55,9 @@ class LedgerException {
     String reason;
 }
 class CommandProcessor {
-    Ledger ledger = new Ledger();
-    void processCommand(String command) {
+    //process commands takes individual commands
+    void processCommand(String command, Ledger ledger) {
         String words[] = command.split(" ");
-        if ("create-ledger".equals(words[0])) {
-            System.out.println("create ledger\n");
-            ledger.name = words[1];
-            int seed = 0;
-            for (int i = 0; i < words.length; i++) {
-                if ("seed".equals(words[i])) {
-                    System.out.println("seed\n");
-                    seed = i;
-                }
-            }
-            ledger.description = words[3];
-            for (int i = 3; i < seed; i++)//TODO CHECK ON ONE WORD DESC
-            {
-                ledger.description += words[i];
-            }
-            ledger.seed = words[seed + 1];
-        }
         if ("create-account".equals(words[0])) {
             System.out.println("create account\n");
             Account account = new Account();
@@ -95,8 +77,7 @@ class CommandProcessor {
                 }
             }
             transaction.note = words[7];
-            for (int i = 7; i < payer; i++)//TODO
-            {
+            for (int i = 7; i < payer; i++){//TODO
                 ledger.description += words[i];
             }
             //TODO process payer at payer+1 and receiver at payer+3
@@ -125,25 +106,65 @@ class CommandProcessor {
             ledger.validate();
         }
     }
-    void processCommandFile(String commandfile) throws IOException {
+    
+    //takes text file, splits it into lines, and feeds to processCommand
+    void processCommandFile(String commandfile) throws IOException, CommandProcessorException {
         String commands = new String(Files.readAllBytes(Paths.get(commandfile)));
+        //remove comment lines
         String lines[] = commands.split("\n");
-        for (String line : lines) {
-            if (line.charAt(0) == '#')
-                continue;
-            processCommand(line);
+        for(int i=0;i<lines.length;i++)
+            if(lines[i].startsWith("#"))
+                lines[i]="";
+        //recombine into single string
+        StringBuilder finalStringBuilder = new StringBuilder("");
+        for(String s:lines){
+            if(!s.equals(""))
+                finalStringBuilder.append(s).append(System.getProperty("line.separator"));
+        }  
+        commands = finalStringBuilder.toString();
+        //ensure that the ledger is created
+        String words[] = commands.split(" ");
+        if ("create-ledger".equals(words[0])) {
+            Ledger ledger = new Ledger();
+            System.out.println("create ledger\n");
+            ledger.name = words[1];
+            int seed = 0;
+            for (int i = 0; i < words.length; i++) {
+                if ("seed".equals(words[i])) {
+                    System.out.println("seed\n");
+                    seed = i;
+                }
+            }
+            ledger.description = words[3];
+            for (int i = 3; i < seed; i++)//TODO CHECK ON ONE WORD DESC
+            {
+                ledger.description += words[i];
+            }
+            ledger.seed = words[seed + 1];
+            
+            //read each line into process command
+            for (String line : lines)
+                processCommand(line, ledger);
+        } else {
+            System.out.println("Must create ledger first!\n");
+            throw new CommandProcessorException(words[0],"must create ledger first");
         }
     }
 }
 
-class CommandProcessorException {
+class CommandProcessorException extends Exception{
     String command;
     String reason;
     int lineNumber;
+    public CommandProcessorException(String c, String r){
+        command = c;
+        reason = r;
+        lineNumber= new Throwable().getStackTrace()[0].getLineNumber();
+    }
 }
 
 public class TestDriver {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, CommandProcessorException {
         CommandProcessor cp = new CommandProcessor();
         cp.processCommandFile("C:\\Users\\Andrew\\Documents\\NetBeansProjects\\ledger\\target\\classes\\com\\csci97\\ledger\\testinput.txt");
     }
