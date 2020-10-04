cf046f9dbfa5dc69086da9b2f8eef4256423f1ae
Andrew Pham <andrewdpham3@gmail.com>
2020-10-03 22:44:15 -0700

    Update ModelService.java

--- ModelService.java
+++ ModelService.java
@@ -32,7 +32,7 @@ class Simulator{
 class VirtualDevice(){
 	String id, type, event;
 	Pair<double, double> location;
-	boolean status, enabled;
+	boolean status, enabled; //TODO INCORPERATE INTO STATE
 	Hashmap<String, String> state;
 	Device d;
 	Simulator s;
@@ -77,35 +77,56 @@ class Person{
 class Controller {
 	Hashmap<Integer, City> cities = new Hashmap();
 	void command(String command){
-		String words[] = command.split(" |\\:");//tokenize delim by spaces and colon
+		//tokenize command by spaces unless in quotes
+		List<String> words = new ArrayList<String>();
+		Matcher m = Pattern.compile("([^\"]\\S*|\".+?\")\\s*").matcher(command);
+		while (m.find())
+    		list.add(m.group(1));
 		words[words.length-1] = words[words.length-1].replace("\n", "").replace("\r", "");//get rid of newline char
-		//define city
-		if ("define".equals(words[0]) && "city".equals(words[1])) {
-			City c = new City();
-			c.setInfo(words[2], words[4], words[6], new Pair<double, double>(Integer.parseInt(words[8]), Integer.parseInt(words[10]));, words[12]);
-			cities.put(words[2], c);
-		}
+		
 		//show
 		if ("show".equals(words[0])) {
 			if("city".equals(words[1])){
 				System.out.println(cities.get(words[2]).getInfo());
 			}
+			if("person".equals(words[1])){
+				
+			}
 		}
 		//define
 		if ("define".equals(words[0])) {
-			VirtualDevice d = new VirtualDevice();
-			cities.get(words[2]).vDevices.add(d);//add to devices list of that city
-			d.defVDevice(words[1], words[3], new Pair<double, double>(Integer.parseInt(words[6]), Integer.parseInt(words[8]));, words[9]);
-			d.setState(words[11], )//TODO BUILD STRING OF TEXT FROM END
+			//city
+			if("city".equals(words[1])){
+				City c = new City();
+				c.setInfo(words[2], words[4], words[6], new Pair<double, double>(Integer.parseInt(words[8]), Integer.parseInt(words[10]));, words[12]);
+				cities.put(words[2], c);
+			}
+			//people
+			else if("resident".equals(words[1])){
+			}
+			else if("visitor".equals(words[1])){
+			}
+			//devices
+			else{
+				VirtualDevice d = new VirtualDevice();
+				cities.get(words[2]).vDevices.add(d);//add to devices list of that city
+				d.defVDevice(words[1], words[3], new Pair<double, double>(Integer.parseInt(words[6]), Integer.parseInt(words[8]));, words[9]);
+				d.setState("text", words[11]);
+			}
 		}
 		//update
 		if ("update".equals(words[0])) {
-			
+			//store first token as key in device state, second as value, repeat
+			for(int i=1;i<words.length;i+=2){
+				cities.get(words[2]).vDevices.
+			}
 		}
 		//simulate event
 		if ("create".equals(words[0]) && "sensor-event".equals(words[2])) {
+			
 		}
-		void event(Event e){
+		void event(Event e) {
+			
 		}
 }
 
